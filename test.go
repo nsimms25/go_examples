c792a8e5e2941fa75142d25687aa8ef001850210
Nick Simms <nick.simms@lumen.com>
2023-09-26 20:49:23 -0700

    Basic strings

--- test.go
+++ test.go
@@ -7,4 +7,8 @@ func main() {
 	var second_string string = "Second string."
 	var combined_strings = first_string + second_string
 	fmt.Println(combined_strings)
+
+	var new_string string = "0123456789"
+	var character_string string = string(new_string[0])
+	fmt.Println(character_string)
 }
