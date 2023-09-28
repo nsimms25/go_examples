7516dcbcd557fcbb498320b81976718ac1d180ac
Nick Simms <nick.simms@lumen.com>
2023-09-27 19:48:33 -0700

    Functionalize

--- test.go
+++ test.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func strings() {
 	var first_string string = "First String, "
 	var second_string string = "Second string."
 	var combined_strings = first_string + second_string
@@ -11,4 +11,8 @@ func main() {
 	var new_string string = "0123456789"
 	var character_string string = string(new_string[0])
 	fmt.Println(character_string)
+}
+
+func main() {
+	strings()
 }
